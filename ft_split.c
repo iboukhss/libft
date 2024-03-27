ba32f99a5ec7415f25dba5fcb3c459ed580bc55c
iboukhss <151830328+iboukhss@users.noreply.github.com>
2024-03-27 01:59:46 +0100

    34/34 done

--- ft_split.c
+++ ft_split.c
@@ -6,10 +6,65 @@
 /*   By: iboukhss <iboukhss@student.42luxe...>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 18:32:15 by iboukhss          #+#    #+#             */
-/*   Updated: 2024/03/22 18:40:55 by iboukhss         ###   ########.fr       */
+/*   Updated: 2024/03/27 00:13:51 by iboukhss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 #include "libft.h"
 
-char	**ft_split(char const *s, char c);
+static size_t	count_tokens(char const *s, char c)
+{
+	size_t	count;
+	int		token;
+
+	count = 0;
+	token = 0;
+	while (*s)
+	{
+		if (*s == c)
+			token = 0;
+		else if (!token)
+		{
+			token = 1;
+			++count;
+		}
+		++s;
+	}
+	return (count);
+}
+
+static void	free_tokens(char **tokens, size_t n)
+{
+	while (n--)
+		free(tokens[n]);
+	free(tokens);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	size_t		count;
+	size_t		i;
+	const char	*start;
+	char		**tokens;
+
+	count = count_tokens(s, c);
+	tokens = ft_calloc(count + 1, sizeof(*tokens));
+	i = 0;
+	while (i < count)
+	{
+		while (*s == c && *s)
+			++s;
+		start = s;
+		while (*s != c && *s)
+			++s;
+		tokens[i] = ft_substr(start, 0, s - start);
+		if (!tokens[i])
+		{
+			free_tokens(tokens, i);
+			return (NULL);
+		}
+		++i;
+	}
+	return (tokens);
+}
